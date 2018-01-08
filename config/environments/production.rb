1b282d78d3fc7356bf14bf7917e63c388861984a
Kaloyan Kostadinov <Kaloyan@users-MacBook-Pro.local>
2018-01-09 00:47:42 +0200

    SMTP::Authentication error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,12 +93,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
   :enable_starttls_auto => true
 }
 end
